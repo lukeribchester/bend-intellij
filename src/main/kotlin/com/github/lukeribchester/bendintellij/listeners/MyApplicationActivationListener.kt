402b58b7e299c002d0b460bc118378e31a433e00
GitHub Action <action@github.com>
2024-05-18 05:55:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/lukeribchester/bendintellij/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/lukeribchester/bendintellij/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.lukeribchester.bendintellij.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
