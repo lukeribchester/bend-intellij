f5d9bd8d47fb93a352e19b1648121e216a085a60
Luke Gareth Ribchester <luke@luke.gr>
2024-05-18 15:22:02 +0200

    wip(lang): define the lexer and parser
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

---
+++ src/main/gen/com/higherorderco/bend/language/parser/BendParser.java
@@ -0,0 +1,49 @@
+// This is a generated file. Not intended for manual editing.
+package com.higherorderco.bend.language.parser;
+
+import com.intellij.lang.PsiBuilder;
+import com.intellij.lang.PsiBuilder.Marker;
+import static com.higherorderco.bend.language.psi.BendTypes.*;
+import static com.intellij.lang.parser.GeneratedParserUtilBase.*;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.tree.TokenSet;
+import com.intellij.lang.PsiParser;
+import com.intellij.lang.LightPsiParser;
+
+@SuppressWarnings({"SimplifiableIfStatement", "UnusedAssignment"})
+public class BendParser implements PsiParser, LightPsiParser {
+
+  public ASTNode parse(IElementType t, PsiBuilder b) {
+    parseLight(t, b);
+    return b.getTreeBuilt();
+  }
+
+  public void parseLight(IElementType t, PsiBuilder b) {
+    boolean r;
+    b = adapt_builder_(t, b, this, null);
+    Marker m = enter_section_(b, 0, _COLLAPSE_, null);
+    r = parse_root_(t, b);
+    exit_section_(b, 0, m, t, r, true, TRUE_CONDITION);
+  }
+
+  protected boolean parse_root_(IElementType t, PsiBuilder b) {
+    return parse_root_(t, b, 0);
+  }
+
+  static boolean parse_root_(IElementType t, PsiBuilder b, int l) {
+    return Program(b, l + 1);
+  }
+
+  /* ********************************************************** */
+  // !<<eof>>
+  static boolean Program(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "Program")) return false;
+    boolean r;
+    Marker m = enter_section_(b, l, _NOT_);
+    r = !eof(b, l + 1);
+    exit_section_(b, l, m, r, false, null);
+    return r;
+  }
+
+}
---
+++ src/main/gen/com/higherorderco/bend/language/psi/BendTypes.java
@@ -0,0 +1,14 @@
+// This is a generated file. Not intended for manual editing.
+package com.higherorderco.bend.language.psi;
+
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.PsiElement;
+import com.intellij.lang.ASTNode;
+
+public interface BendTypes {
+
+
+
+  class Factory {
+  }
+}
